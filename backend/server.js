cd8cdd2eea36997e690475ff4d2dfa03a4b775a8
wayneleon1 <rurangwaleon10@gmail.com>
2025-08-20 18:30:37 +0200

    Implemented transaction summary endpoint

--- backend/server.js
+++ backend/server.js
@@ -62,6 +62,33 @@ app.delete("/api/transactions/:id", async (req, res) => {
   }
 });
 
+app.get("/api/transactions/summary/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const balanceResult = await sql`
+     SELECT COALESCE(SUM(amount),0) as balance FROM transactions WHERE user_id = ${userId}
+    `;
+
+    const incomeResult = await sql`
+     SELECT COALESCE(SUM(amount),0) as income FROM transactions WHERE user_id = ${userId} AND amount > 0
+    `;
+
+    const expensesResult = await sql`
+     SELECT COALESCE(SUM(amount),0) as expenses FROM transactions WHERE user_id = ${userId} AND amount < 0
+    `;
+
+    res.status(200).json({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expensesResult[0].expenses,
+    });
+  } catch (error) {
+    console.log("Error getting the summary", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/api/transactions", async (req, res) => {
   try {
     const { title, amount, category, user_id } = req.body;
