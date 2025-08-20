3aa361e6d2ae7897685d208c59dc17ba7ca422e1
wayneleon1 <rurangwaleon10@gmail.com>
2025-08-20 16:43:39 +0200

    Installed dependencies and configured them

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+const initDB = async () => {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+    console.log("Database initializing successfully");
+  } catch (error) {
+    console.log("Error initialing DB", error);
+    process.exit(1);
+  }
+};
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on:", PORT);
+  });
+});
