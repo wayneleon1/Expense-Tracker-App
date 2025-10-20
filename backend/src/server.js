5a160ba8f714918de47ada6d15b43ed2645c7537
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-20 22:17:35 +0200

    Implement cron job on server js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,23 @@ import dotenv from "dotenv";
 import express, { json } from "express";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRouters from "./routes/transactionsRoutes.js";
+import job from "./config/cron.js";
+
 import { initDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
+if (process.env.NODE_ENV === "production") job.start();
+
 app.use(rateLimiter);
 app.use(json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (res, req) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRouters);
 
