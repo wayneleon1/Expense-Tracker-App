9ce7d8c94ca2fd90f91c773ef4c187dbebf649cf
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-22 09:17:47 +0200

    Implemented PageLoader component

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -4,6 +4,7 @@ import { Text, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
 import { useTransactions } from "../../hooks/useTransactions";
 import { useEffect } from "react";
+import PageLoader from "../../components/PageLoader";
 
 export default function Page() {
   const { user } = useUser();
@@ -13,6 +14,8 @@ export default function Page() {
   useEffect(() => {
     loadData();
   }, [loadData]);
+
+  if (loading) return <PageLoader />;
 
   return (
     <View>
